fb098187467a7201392e69e3873cd38eb2ae4c44
Luís Martinho <lmartinho@gmail.com>
2018-06-01 15:01:58 +0100

    Add the TypeScript hello world

---
+++ typescript/hello.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Olá, " + person;
+}
+//let user = [0, 1, 2];
+var user = "Luís User";
+document.body.innerHTML = greeter(user);
---
+++ typescript/hello.ts
@@ -0,0 +1,8 @@
+function greeter(person: string) {
+    return "Olá, " + person;
+}
+
+//let user = [0, 1, 2];
+let user = "Luís User";
+
+document.body.innerHTML = greeter(user);
