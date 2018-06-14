7840c2ee174acd2e8e25a499758e7851f2799b62
Luís Martinho <lmartinho@gmail.com>
2018-06-15 00:13:06 +0100

    Document some of the instructions in the x86 asm 
    Trying to breakdown what's actually going on here.

--- asm/hello_32.asm
+++ asm/hello_32.asm
@@ -1,21 +1,23 @@
+; Write msg to stdout
+; user_ssize_t write(int fd, user_addr_t cbuf, user_size_t nbyte);
+; @see: https://opensource.apple.com/source/xnu/xnu-3248.60.10/bsd/kern/syscalls.master.auto.html
 global start
 
 section .text
 start:
-    push dword msg.len
-    push dword msg
-    push dword 1
-    mov eax, 4
-    sub esp, 4
-    int 0x80
-    add esp, 16
+    push dword msg.len ; user_size_t nbyte
+    push dword msg     ; user_addr_t cbuf
+    push dword 1       ; int fd
+    mov eax, 4         ; the system interprets 4 as "write"
+    sub esp, 4         ; required for BSD, so can do the int 0x80 without copying args around
+    int 0x80           ; call kernel function (passes control to the interrupt handler @see: https://en.wikipedia.org/wiki/Interrupt_vector_table)
+    add esp, 16        ; ?
 
-    push dword 0
-    mov eax, 1
-    sub esp, 12
-    int 0x80
+    push dword 0       ; rval
+    mov eax, 1         ; exit
+    sub esp, 12        ; (for BSD, see above)
+    int 0x80           ; call kernel function
 
 section .data
-
 msg:    db  "Olá, mundo!", 10
 .len:   equ $ - msg
