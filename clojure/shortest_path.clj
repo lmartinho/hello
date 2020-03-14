1219440646467a684b8c5cbe0b1b0d0ea0fe5085
Luís Martinho <lmartinho@gmail.com>
2020-03-14 21:52:05 +0000

    Make the shortest path recursive function self-contained
    The `shortest_path` is now just a cleaner interface for the actual recursive call.

--- clojure/shortest_path.clj
+++ clojure/shortest_path.clj
@@ -11,8 +11,6 @@
     :e []
 })
 
-(declare shortest_path)
-
 (defn shortest [a b]
     (if (< (count a) (count b))
         a
@@ -33,24 +31,26 @@
 (defn shortest_path_aux [nodes node2]
     (if (empty? nodes)
         []
-        (shortest_non_empty 
-            (shortest_path (first nodes) node2)
+        (shortest_non_empty
+            (let [node1 (first nodes)]
+                (if (= node1 node2)
+                    [node2]
+                    (if (empty? (graph node1))
+                        []
+                        (concat
+                            [node1]
+                            (shortest_path_aux (graph node1) node2)
+                        )
+                    )
+                )
+            )
             (shortest_path_aux (rest nodes) node2)
         )
     )
 )
 
 (defn shortest_path [node1 node2]
-    (if (= node1 node2)
-        [node2]
-        (if (empty? (graph node1))
-            []
-            (concat
-                [node1]
-                (shortest_path_aux (graph node1) node2)
-            )
-        )
-    )
+    (shortest_path_aux [node1] node2)
 )
 
 (print (shortest_path :a :e))
