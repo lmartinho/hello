daee70841020dd90181a24ecc76afa8157ef9ae9
Luís Martinho <lmartinho@gmail.com>
2020-03-14 19:49:39 +0000

    Add working shortest path in Clojure

--- clojure/shortest_path.clj
+++ clojure/shortest_path.clj
@@ -11,14 +11,46 @@
     :e []
 })
 
-(defn shortest [node1 node2] 
-    (if (= node1 node2) 
-        [node2]
-        (concat
-            [node1]
-            (shortest (first (graph node1)) node2)
+(declare shortest_path)
+
+(defn shortest [a b]
+    (if (< (count a) (count b))
+        a
+        b
+    )
+)
+
+(defn shortest_non_empty [a b]
+    (if (empty? a)
+        b
+        (if (empty? b)
+            a 
+            (shortest a b)
         )
     )
 )
 
-(print (shortest :a :e))
+(defn shortest_path_aux [nodes node2]
+    (if (empty? nodes)
+        []
+        (shortest_non_empty 
+            (shortest_path (first nodes) node2)
+            (shortest_path_aux (rest nodes) node2)
+        )
+    )
+)
+
+(defn shortest_path [node1 node2]
+    (if (= node1 node2)
+        [node2]
+        (if (empty? (graph node1))
+            []
+            (concat
+                [node1]
+                (shortest_path_aux (graph node1) node2)
+            )
+        )
+    )
+)
+
+(print (shortest_path :a :e))
